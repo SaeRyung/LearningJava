99794a235da1f7b2a83e1c4dfa56625ca4ddf5ad
SaeRyung <101532588+SaeRyung@users.noreply.github.com>
2024-05-29 21:13:10 +0900

    묵시적 변환 완료

--- src/exercise/chapter_32/fish/seaSituation.java
+++ src/exercise/chapter_32/fish/seaSituation.java
@@ -33,5 +33,19 @@ public class seaSituation {
 
         //핵심 타입
         //묵시적 형 변환
+        //1.타입 선언: 부모, 인스턴스화: 부모
+        //2.타입선언: 부모, 인스턴스화: 자식
+        //3.타입선언:자식, 인스턴스화:자식
+        //4.타입선언:자식, 인스턴스화:부모 => 묵시적 변환 X
+        Fish fish1 = new Fish(); //1
+        fish1.eat("새우");
+
+        Fish fish2 = new FishChild();//2_오버라이드
+        fish2.eat("새우");
+
+        Fish fish3 = new FishChild();//3
+        fish3.eat("새우");
+
+        //FishChile fish4 = new Fish() //4. error
     }
 }
