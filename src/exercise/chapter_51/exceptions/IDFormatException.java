c32f70676ca1747c8a7f4244f558d82ad318c3fe
SaeRyung <sr1094@naver.com>
2024-06-11 14:08:39 +0900

    예외처리 사용

---
+++ src/exercise/chapter_51/exceptions/IDFormatException.java
@@ -0,0 +1,10 @@
+package exercise.chapter_51.exceptions;
+
+//RuntimeException
+public class IDFormatException extends RuntimeException {
+
+
+    public IDFormatException(String message) {
+        super(message);
+    }
+}
